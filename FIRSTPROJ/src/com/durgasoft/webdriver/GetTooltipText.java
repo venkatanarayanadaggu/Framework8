05e02adc642edb17fac6b10b7b10a4629a82837d
User <User@Dell>
2019-12-09 23:22:37 +0530

    new code

--- FIRSTPROJ/src/com/durgasoft/webdriver/GetTooltipText.java
+++ FIRSTPROJ/src/com/durgasoft/webdriver/GetTooltipText.java
@@ -8,6 +8,7 @@ public class GetTooltipText {
 	public static void main(String[] args)throws InterruptedException {  
 	System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\Desktop\\selenium\\chromedriver.exe");
 	WebDriver driver= new ChromeDriver();
+	//this is google page
 	driver.get("https://www.google.com/advanced_search");
 	driver.manage().window().maximize();
 	Thread.sleep(2000);
